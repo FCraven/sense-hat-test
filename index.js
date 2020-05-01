4f2ddaa37ff36d332bf5bdf7ee88b39b50ee434c
FCraven <craven.francis@gmail.com>
2020-05-01 16:22:48 -0400

    test run rainbow

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const matrix = require('node-sense-hat').Leds;
 
 const O = [0, 0, 0];
 const X = [255, 0, 0];
+const red = [255, 0, 0]
+const orange = [255, 165, 0];
+const yellow = [255, 215, 0]
+const green = [0, 255, 0]
+const blue = [0, 0, 255]
+const indigo = [75, 0, 130]
+const violet = [128, 0, 128]
 
-const cross = [
+
+const bigX = [
   X, O, O, O, O, O, O, X,
   O, X, O, O, O, O, X, O,
   O, O, X, O, O, X, O, O,
@@ -14,5 +22,25 @@ const cross = [
   X, O, O, O, O, O, O, X,
 ];
 
-matrix.setPixels(cross);
+const cross = [
+  red, red, red, red, red, red, red, red,
+  orange, orange, orange, orange, orange, orange, orange, orange,
+  yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
+  green, green, green, green, green, green, green, green,
+  blue, blue, blue, blue, blue, blue, blue, blue,
+  indigo, indigo, indigo, indigo, indigo, indigo, indigo, indigo,
+  violet, violet, violet, violet, violet, violet, violet, violet,
+  O, O, O, O, O, O, O, O,
+]
 
+matrix.setPixels(bigX);
+matrix.clear([127, 0, 0])
+
+matrix.setPixels(cross)
+matrix.clear([127, 0, 0])
+matrix.setPixels(cross)
+matrix.clear([127, 0, 0])
+matrix.setPixels(cross)
+matrix.clear([127, 0, 0])
+matrix.setPixels(cross)
+matrix.clear([127, 0, 0])
